d1d681e4657fb5b6dea7e3b1dedc5dc17b891bcc
Christian Peth <c@peth.cloud>
2025-11-24 18:33:18 -0800

    added unit test for ExtensionUriHandler

---
+++ extension/test/unit/general/ExtensionUriHandler.unit.test.ts
@@ -0,0 +1,153 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { ExtensionUriHandler } from '../../../src/ExtensionUriHandler';
+import { IFabricExtensionServiceCollection } from '@microsoft/vscode-fabric-api';
+import { FabricUriHandler, IConfigurationProvider, IFabricEnvironmentProvider, ILogger } from '@microsoft/vscode-fabric-util';
+import { WorkspaceManager } from '../../../src/workspace/WorkspaceManager';
+import { MockFabricEnvironmentProvider } from './serviceCollection';
+
+describe('ExtensionUriHandler', () => {
+    let sandbox: sinon.SinonSandbox;
+    let configEmitters: vscode.EventEmitter<string>[];
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+        configEmitters = [];
+
+        sandbox.stub(vscode.l10n, 't').callsFake((messageOrOptions: any, ...params: any[]) => {
+            if (typeof messageOrOptions === 'string') {
+                const message = messageOrOptions;
+                if (params.length === 0) {
+                    return message;
+                }
+
+                return message.replace(/\{(\d+)\}/g, (_match: string, index: string) => {
+                    const numericIndex = Number(index);
+                    return params[numericIndex] ?? '';
+                });
+            }
+
+            if (typeof messageOrOptions === 'object' && messageOrOptions?.message) {
+                const message: string = messageOrOptions.message;
+                const args = Array.isArray(messageOrOptions.args) ? messageOrOptions.args : [];
+                if (args.length === 0) {
+                    return message;
+                }
+
+                return message.replace(/\{(\d+)\}/g, (_match: string, index: string) => {
+                    const numericIndex = Number(index);
+                    return (args as any[])[numericIndex] ?? '';
+                });
+            }
+
+            return '';
+        });
+    });
+
+    afterEach(() => {
+        configEmitters.forEach(emitter => emitter.dispose());
+        sandbox.restore();
+    });
+
+    function createConfigProvider(): IConfigurationProvider {
+        const emitter = new vscode.EventEmitter<string>();
+        configEmitters.push(emitter);
+        return {
+            get: <T>(_key: string, defaultValue: T) => defaultValue,
+            update: async () => { /* no-op */ },
+            onDidConfigurationChange: emitter.event,
+        };
+    }
+
+    function createLogger(infoSpy?: sinon.SinonSpy): ILogger {
+        const spy = infoSpy ?? sandbox.spy();
+        return {
+            trace: () => { /* no-op */ },
+            debug: () => { /* no-op */ },
+            info: spy,
+            warn: () => { /* no-op */ },
+            error: () => { /* no-op */ },
+            show: () => { /* no-op */ },
+            log: () => { /* no-op */ },
+            reportExceptionTelemetryAndLog: () => { /* no-op */ },
+        };
+    }
+
+    function createHandler(overrides?: {
+        refreshStub?: sinon.SinonStub<[], Promise<void>>;
+        loggerInfoSpy?: sinon.SinonSpy;
+        fabricEnvironmentProvider?: IFabricEnvironmentProvider;
+        configProvider?: IConfigurationProvider;
+    }): {
+            handler: ExtensionUriHandler;
+            refreshStub: sinon.SinonStub<[], Promise<void>>;
+            infoSpy: sinon.SinonSpy;
+            core: IFabricExtensionServiceCollection;
+        } {
+        const refreshStub = overrides?.refreshStub ?? sandbox.stub<[], Promise<void>>().resolves();
+        const workspaceManager = { refreshConnectionToFabric: refreshStub } as unknown as WorkspaceManager;
+        const core: IFabricExtensionServiceCollection = {
+            workspaceManager,
+            artifactManager: {} as any,
+            apiClient: {} as any,
+        };
+        const infoSpy = overrides?.loggerInfoSpy ?? sandbox.spy();
+        const handler = new ExtensionUriHandler(
+            core,
+            null,
+            createLogger(infoSpy),
+            overrides?.fabricEnvironmentProvider ?? new MockFabricEnvironmentProvider(),
+            overrides?.configProvider ?? createConfigProvider()
+        );
+        return { handler, refreshStub, infoSpy, core };
+    }
+
+    it('handles signup completion callback with auto-assigned license', async () => {
+        const executeCommandStub = sandbox.stub(vscode.commands, 'executeCommand').resolves(undefined);
+        const showInformationStub = sandbox.stub(vscode.window, 'showInformationMessage').resolves(undefined);
+
+        const { handler, refreshStub, infoSpy } = createHandler();
+
+        const uri = vscode.Uri.parse('vscode-fabric://signup?signedUp=1&autoAssigned=1');
+        await handler.handleUri(uri);
+
+        assert.strictEqual(refreshStub.calledOnce, true, 'refreshConnectionToFabric should be invoked when signup completes');
+        assert.strictEqual(executeCommandStub.calledOnce, true, 'workbench view should be shown');
+        assert.deepStrictEqual(executeCommandStub.firstCall.args, ['workbench.view.extension.vscode-fabric_view_workspace']);
+
+        const expectedMessage = "We've assigned you a Microsoft Fabric (Free) license for personal use. You're signed in and can create and explore Fabric items. [Learn More...](https://aka.ms/fabric-trial)";
+        assert.strictEqual(showInformationStub.calledOnce, true, 'informational message should be shown');
+        assert.strictEqual(showInformationStub.firstCall.args[0], expectedMessage, 'auto-assigned license message should be used');
+        assert.strictEqual(infoSpy.calledWith('Signup completion callback received'), true, 'logger should record signup completion');
+    });
+
+    it('handles signup completion callback without auto-assigned license', async () => {
+        sandbox.stub(vscode.commands, 'executeCommand').resolves(undefined);
+        const showInformationStub = sandbox.stub(vscode.window, 'showInformationMessage').resolves(undefined);
+
+        const { handler, refreshStub } = createHandler();
+
+        const uri = vscode.Uri.parse('vscode-fabric://signup?signedUp=1');
+        await handler.handleUri(uri);
+
+        assert.strictEqual(refreshStub.calledOnce, true, 'refreshConnectionToFabric should be invoked when signup completes');
+        const expectedMessage = 'Welcome to Microsoft Fabric! Your account setup is complete.';
+        assert.strictEqual(showInformationStub.calledOnce, true, 'information message should be shown');
+        assert.strictEqual(showInformationStub.firstCall.args[0], expectedMessage, 'welcome message should be used when license is not auto-assigned');
+    });
+
+    it('delegates to base handler when URI is not a signup callback', async () => {
+        const baseHandleStub = sandbox.stub(FabricUriHandler.prototype, 'handleUri').resolves();
+        const { handler } = createHandler();
+
+        const uri = vscode.Uri.parse('vscode-fabric://signup?artifactId=12345');
+        await handler.handleUri(uri);
+
+        assert.strictEqual(baseHandleStub.calledOnce, true, 'base handler should be invoked for non-signup URIs');
+        assert.strictEqual(baseHandleStub.firstCall.args[0], uri, 'base handler should receive the original URI');
+    });
+});
