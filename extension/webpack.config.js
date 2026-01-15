358aeab09ad05c0310b5cd62b3b94c75624c791c
Mike Wade <mwade@microsoft.com>
2026-01-15 07:51:45 -0800

    Create a simple "Hello web" extension

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -59,4 +59,50 @@ const extensionConfig = {
         level: 'log', // enables logging required for problem matchers
     },
 };
-module.exports = [extensionConfig];
+
+/** @type WebpackConfig */
+const webExtensionConfig = {
+    target: 'webworker',
+    mode: 'none',
+    entry: './src/web/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist/web'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2'
+    },
+    externals: {
+        vscode: 'commonjs vscode',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@microsoft/vscode-fabric-api': path.resolve(__dirname, '../api/src/'),
+            '@microsoft/vscode-fabric-util': path.resolve(__dirname, '../util/src/'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/, /out\/\.test-extensions/],
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            getCustomTransformers: (/** @type {any} */ program) => di.di({program})
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    watchOptions: {
+        ignored: ['**/out/.test-extensions/**']
+    },
+    devtool: 'nosources-source-map',
+    infrastructureLogging: {
+        level: 'log',
+    },
+};
+
+module.exports = [extensionConfig, webExtensionConfig];
