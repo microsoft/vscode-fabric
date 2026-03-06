b3d3ac8f89585e793d81b2133b4779867c5697eb
Mike Wade <mwade@microsoft.com>
2026-03-06 14:15:17 -0800

    Create a OneLakeDfsClient which can read from OneLake

---
+++ extension/src/onelake/OneLakeDfsClient.ts
@@ -0,0 +1,77 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { IFabricApiClient } from '@microsoft/vscode-fabric-api';
+import { stringToUint8Array } from '../bufferUtilities';
+import { succeeded } from '../utilities';
+
+export class OneLakeDfsClient {
+    private static readonly defaultDfsEndpoint = 'https://onelake.dfs.fabric.microsoft.com';
+
+    public constructor(
+        private readonly apiClient: IFabricApiClient,
+        private readonly dfsEndpoint: string = OneLakeDfsClient.defaultDfsEndpoint
+    ) {
+    }
+
+    public async readFile(workspaceId: string, lakehouseId: string, filePath: string): Promise<Uint8Array> {
+        const response = await this.apiClient.sendRequest({
+            url: this.buildFileUrl(workspaceId, lakehouseId, filePath),
+            method: 'GET',
+            streamResponseStatusCodes: new Set([200]),
+        });
+
+        if (!succeeded(response)) {
+            throw new Error(`OneLake readFile failed with status ${response.status}`);
+        }
+
+        const browserStream = (response.response as any)?.browserStreamBody as ReadableStream<Uint8Array> | undefined;
+        if (browserStream) {
+            return await this.readBrowserStream(browserStream);
+        }
+
+        if (response.bodyAsText !== undefined) {
+            return stringToUint8Array(response.bodyAsText);
+        }
+
+        return new Uint8Array(0);
+    }
+
+    private buildFileUrl(workspaceId: string, lakehouseId: string, filePath: string): string {
+        const baseEndpoint = this.dfsEndpoint.replace(/\/+$/, '');
+        const normalizedPath = filePath.split('/').filter(segment => segment.length > 0).map(encodeURIComponent).join('/');
+        const encodedWorkspaceId = encodeURIComponent(workspaceId);
+        const encodedLakehouseId = encodeURIComponent(lakehouseId);
+        const pathSuffix = normalizedPath ? `/${normalizedPath}` : '';
+
+        return `${baseEndpoint}/${encodedWorkspaceId}/${encodedLakehouseId}/Files${pathSuffix}`;
+    }
+
+    private async readBrowserStream(stream: ReadableStream<Uint8Array>): Promise<Uint8Array> {
+        const reader = stream.getReader();
+        const chunks: Uint8Array[] = [];
+        let totalLength = 0;
+
+        while (true) {
+            const { done, value } = await reader.read();
+            if (done) {
+                break;
+            }
+
+            if (value) {
+                chunks.push(value);
+                totalLength += value.length;
+            }
+        }
+
+        const result = new Uint8Array(totalLength);
+        let offset = 0;
+
+        for (const chunk of chunks) {
+            result.set(chunk, offset);
+            offset += chunk.length;
+        }
+
+        return result;
+    }
+}
